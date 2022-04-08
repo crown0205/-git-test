331c85555e723471bcb597f3a7c837ee9ad54b32
JONGGIL LEE <brianlee2k@naver.com>
2022-04-08 17:37:36 +0900

    branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>test</p>
+      <p>test 2</p>
     </div>
   );
 }
